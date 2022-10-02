a667ed67ba65cbacbe7876aa74508f1299e8f3e6
TimuDevGroup <113307422+nortewebapp@users.noreply.github.com>
2022-10-02 20:06:28 -0300

    se actualiza fecha

--- js/app-carritos.js
+++ js/app-carritos.js
@@ -27,7 +27,7 @@ function mostrarArreglos(arreglos) {
   <a href="https://api.whatsapp.com/send?phone=543435199134&text=Hola!%20quisiera%20avisar%20que%20faltan%20publicaciones%20en%20el%20carrito!%F0%9F%92%BC%F0%9F%93%96%F0%9F%93%97%F0%9F%93%98%F0%9F%93%9A" target="_blank"><button type="button" class="btn btn-primary btn-lg btn-block mb-1" target="_blank">Avisar falta de publicaciones</button></a>
   <a href="https://forms.gle/Wj29m5KWW1Z17HT78" target="_blank"><button type="button" class="btn btn-primary btn-lg btn-block mb-1" target="_blank">Queres contar una experiencia? hacelo aqui!</button></a>  
   <br>
-  <h4>SEMANA DE 26/09/2022</h4>
+  <h4>SEMANA DE 03/10/2022</h4>
   <table class="table">
                   <thead>
                       <tr>
