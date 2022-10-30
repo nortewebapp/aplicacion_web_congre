348ec252ab5acb2784c506ef4998bdc2531e5cc1
TimuDevGroup <113307422+nortewebapp@users.noreply.github.com>
2022-10-30 19:02:58 -0300

    reseteo para semana del 31/10

--- js/app-carritos.js
+++ js/app-carritos.js
@@ -27,7 +27,7 @@ function mostrarArreglos(arreglos) {
 
   <br>
   <div class="container">
-        <h4 class="text-center">Semana del 24/10/2022</h4>
+        <h4 class="text-center">Semana del 31/10/2022</h4>
         <table class="table">
             
             <thead>
