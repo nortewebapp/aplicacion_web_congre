e99d0b635182bbc75680f4468f8bb8f1997c1139
TimuDevGroup <113307422+nortewebapp@users.noreply.github.com>
2023-02-19 18:30:13 -0300

    cambios de 20/02

--- js/api-clima.js
+++ js/api-clima.js
@@ -1,33 +1,66 @@
-const contClima = document.getElementById("clima")
+const API_KEY = '3d3b9b32aebc72e5e766753be6d6e4d5';
+
+const resultContainer = document.getElementById('clima');
 
 
-// Solicitud GET (Request).
-fetch('https://api.tutiempo.net/json/?lan=es&apid=a5Gq4X4aazq7Wdc&lid=43214')
-  .then(response => response.json())
-  .then(data => console.log(data))
 
+
+const getWeatherData = (position) => {
+  const { latitude, longitude } = position.coords;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      showWeatherInfo([data]);
+    });
+};
+
+const getWeatherDataByRegion = (region) => {
+  const url = `https://api.openweathermap.org/data/2.5/find?q=${region}&appid=${API_KEY}&units=metric`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      showWeatherInfo(data.list);
+    });
+};
 
 const showWeatherInfo = (data) => {
-    console.log(data);
-    let result = ``;
-  
-    for (item of data) {
-      result += `<div class="row alert alert-light mx-2 my-4">
-                      <div class="col-1">
-                          <img src="https://openweathermap.org/img/wn/${item.weather[0].icon}.png" />
-                      </div>
-                      <ul>
-                          <li>Región: ${item.name}</li>
-                          <li>Temperatura: ${item.main.temp}</li>
-                          <li>Sensación termica: ${item.main.feels_like}</li>
-                          <li>Humedad: ${item.main.humidity}</li>
-                          <li>Presión:  ${item.main.pressure}</li>
-                          <li>Velocidad del viento:  ${item.wind.speed}</li>
-                          <li>País: ${item.sys.country}</li>
-                      </ul>
-                  </div>`;
-    }
-  
-    contClima.innerHTML = result;
-  };
+  console.log(data);
+  let result = ``;
 
+  for (item of data) {
+    result += `<div class="row alert-light mx-4 my-4 justify-content-center ">
+                    <h3 >El clima en Parana</h3>
+                    <ul class='clima col-12'>
+                        <li>Región: ${item.name}</li>
+                        <li>Temperatura: ${item.main.temp}</li>
+                        <li>Sensación termica: ${item.main.feels_like}</li>
+                        <li>Humedad: ${item.main.humidity}</li>
+                        <li>Presión:  ${item.main.pressure}</li>
+                        <li>Velocidad del viento:  ${item.wind.speed}</li>
+                    </ul>
+                </div>`;
+  }
+
+  resultContainer.innerHTML = result;
+};
+
+const handleGetPositionError = () => {
+  console.log('No se ha podido geolocalizar al usuario...');
+};
+
+const getUserPosition = () => {
+  navigator.geolocation.getCurrentPosition(
+    getWeatherData,
+    handleGetPositionError
+  );
+};
+
+getUserPosition();
