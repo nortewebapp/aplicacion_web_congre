36a12dd811c20064acdab854d1deb219718e3802
TimuDevGroup <113307422+nortewebapp@users.noreply.github.com>
2023-04-23 20:59:20 -0300

    24/04

--- js/app-carritos.js
+++ js/app-carritos.js
@@ -27,7 +27,7 @@ function mostrarArreglos(arreglos) {
 
   <br>
   <div class="container">
-        <h4 class="text-center">Semana del 17/04/2023</h4>
+        <h4 class="text-center">Semana del 24/04/2023</h4>
         <table class="table">
             
             <thead>
