e3b28a0dbb28ed9853d18804079381334a92cc40
nortewebapp <113307422+nortewebapp@users.noreply.github.com>
2023-07-02 19:47:05 -0300

    Update app-carritos.js
    cambio fecha

--- js/app-carritos.js
+++ js/app-carritos.js
@@ -27,7 +27,7 @@ function mostrarArreglos(arreglos) {
 
   <br>
   <div class="container">
-        <h4 class="text-center">Semana del 18/06/2023</h4>
+        <h4 class="text-center">Semana del 02/07/2023</h4>
         <table class="table">
             
             <thead>
